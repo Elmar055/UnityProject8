30e7f1eecfa98a83a9b5bf9f3b664cca3aa3850d
Elmar055 <104317952+Elmar055@users.noreply.github.com>
2023-03-15 16:12:05 +0400

    All files upload

---
+++ SoccerScripting/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    // enemy speed
+    public float speed;
+    // for get rigidbody component
+    private Rigidbody enemyRb;
+
+    private GameObject playerGoal;
+
+    private SpawnManagerX spawnManagerXScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // assign values to variables
+        enemyRb = GetComponent<Rigidbody>();
+
+        playerGoal = GameObject.Find("Player Goal");
+        spawnManagerXScript = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        speed = spawnManagerXScript.enemySpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
